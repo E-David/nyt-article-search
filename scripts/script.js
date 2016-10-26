5b00a2b40f0797d16bb869c46fb3046fbe83a232
Ryan Meeker <ryanjmeeker@gmail.com>
2016-10-26 11:06:11 -0500

    added smiley

--- scripts/script.js
+++ scripts/script.js
@@ -55,7 +55,7 @@ var Controller = Backbone.Router.extend({ // Use this exact syntax. Accesses "Ro
 		"*default": "handleDefault" //when the url contains any hash pattern not found (indicated by *) invoke the handleDefault method
 	},
 	handleHome: function(){
-		console.log("Welcome home") // simple test to ensure handleHome is invoked (not required)
+		console.log("Welcome home") // simple test to ensure handleHome is invoked (not required) :D 
 		var articleCollection = new ArticleCollection(), // this creates an instance of the ArticleCollection constructor
 			promise = articleCollection.fetch() // invokes Backbone fetch method which requests data from url in ArticleCollection constructor seen above (line 21) assigns return value to variable promise
 		
